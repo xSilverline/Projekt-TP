30a09f73e4b0fb35fcaff9f96abcf00144c5c9cf
xSilverline <32816233+xSilverline@users.noreply.github.com>
2017-12-04 10:57:20 +0100

    Update_3 C-S

--- src/SetGui.java
+++ src/SetGui.java
@@ -1,7 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class SetGui
 {
+    JFrame frame = new JFrame("Game");
+    JLabel messageLabel = new JLabel("");
+    ImageIcon icon;
+    ImageIcon opponentIcon;
+
+
     SetGui()
     {
+        messageLabel.setBackground(Color.lightGray);
+        frame.getContentPane().add(messageLabel, "South");
+
+        JPanel boardPanel = new JPanel();
+        boardPanel.setBackground(Color.black);
+        boardPanel.setLayout(new GridLayout(3, 3, 2, 2));
+
+        frame.getContentPane().add(boardPanel, "Center");
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(240, 160);
+        frame.setVisible(true);
+        frame.setResizable(true);
 
     }
 }
--- src/client.java
+++ src/client.java
@@ -10,13 +10,9 @@ import java.net.Socket;
 
 public class Client {
 
-    private JFrame frame = new JFrame("Game");
-    private JLabel messageLabel = new JLabel("");
-    private ImageIcon icon;
-    private ImageIcon opponentIcon;
 
-    private Square[] board = new Square[9];
-    private Square currentSquare;
+    SetGui setGui;
+
 
     private static int PORT = 9090;
     private Socket socket;
@@ -36,22 +32,7 @@ public class Client {
         out = new PrintWriter(socket.getOutputStream(), true);
 
         // Layout GUI
-        messageLabel.setBackground(Color.lightGray);
-        frame.getContentPane().add(messageLabel, "South");
-
-        JPanel boardPanel = new JPanel();
-        boardPanel.setBackground(Color.black);
-        boardPanel.setLayout(new GridLayout(3, 3, 2, 2));
-        for (int i = 0; i < board.length; i++) {
-            final int j = i;
-            board[i] = new Square();
-            board[i].addMouseListener(new MouseAdapter() {
-                public void mousePressed(MouseEvent e) {
-                    currentSquare = board[j];
-                    out.println("MOVE " + j);}});
-            boardPanel.add(board[i]);
-        }
-        frame.getContentPane().add(boardPanel, "Center");
+        setGui = new SetGui();
     }
 
     /**
@@ -73,32 +54,18 @@ public class Client {
             response = in.readLine();
             if (response.startsWith("WELCOME")) {
                 char mark = response.charAt(8);
-                icon = new ImageIcon(mark == 'X' ? "x.gif" : "o.gif");
-                opponentIcon  = new ImageIcon(mark == 'X' ? "o.gif" : "x.gif");
-                frame.setTitle("Tic Tac Toe - Player " + mark);
+                setGui.icon = new ImageIcon(mark == 'X' ? "x.gif" : "o.gif");
+                setGui.opponentIcon  = new ImageIcon(mark == 'X' ? "o.gif" : "x.gif");
+                setGui.frame.setTitle("Player " + mark);
             }
-            while (true) {
+            while (true)
+            {
                 response = in.readLine();
-                if (response.startsWith("VALID_MOVE")) {
-                    messageLabel.setText("Valid move, please wait");
-                    currentSquare.setIcon(icon);
-                    currentSquare.repaint();
-                } else if (response.startsWith("OPPONENT_MOVED")) {
-                    int loc = Integer.parseInt(response.substring(15));
-                    board[loc].setIcon(opponentIcon);
-                    board[loc].repaint();
-                    messageLabel.setText("Opponent moved, your turn");
-                } else if (response.startsWith("VICTORY")) {
-                    messageLabel.setText("You win");
-                    break;
-                } else if (response.startsWith("DEFEAT")) {
-                    messageLabel.setText("You lose");
-                    break;
-                } else if (response.startsWith("TIE")) {
-                    messageLabel.setText("You tied");
+                if(response.startsWith("X"))
+                {
                     break;
                 } else if (response.startsWith("MESSAGE")) {
-                    messageLabel.setText(response.substring(8));
+                    setGui.messageLabel.setText(response.substring(8));
                 }
             }
             out.println("QUIT");
@@ -109,11 +76,11 @@ public class Client {
     }
 
     private boolean wantsToPlayAgain() {
-        int response = JOptionPane.showConfirmDialog(frame,
+        int response = JOptionPane.showConfirmDialog(setGui.frame,
                 "Want to play again?",
                 "Tic Tac Toe is Fun Fun Fun",
                 JOptionPane.YES_NO_OPTION);
-        frame.dispose();
+        setGui.frame.dispose();
         return response == JOptionPane.YES_OPTION;
     }
 
@@ -143,10 +110,6 @@ public class Client {
            // SetupWindow setupWindow = new SetupWindow();
             String serverAddress = (args.length == 0) ? "localhost" : args[1];
             Client client = new Client(serverAddress);
-            client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            client.frame.setSize(240, 160);
-            client.frame.setVisible(true);
-            client.frame.setResizable(true);
             client.play();
             if (!client.wantsToPlayAgain()) {
                 break;
